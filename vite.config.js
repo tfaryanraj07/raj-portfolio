c5becaeee71ea10245d8cc4722b46845537c6aa0
raj <rsolanki9235@gmail.com>
2026-02-01 15:19:25 +0530

    fix: remove base path for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,6 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// export default defineConfig({
-//   base: '/raj-portfolio/',  // GitHub repo name
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Use production base only when building for production (e.g. GitHub Pages).
-// During `npm run dev`, base will be '/' which keeps local dev server working.
-export default defineConfig(({ mode }) => {
-  const isProd = mode === 'production'
-  return {
-    base: isProd ? '/raj-portfolio/' : '/',
-    plugins: [react()],
-  }
-})
-
-
+export default defineConfig({
+  plugins: [react()],
+});
